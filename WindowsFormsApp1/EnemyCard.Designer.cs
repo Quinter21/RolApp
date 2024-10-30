d50c539630618bc65f2f48a87f63e0202959d4f0
Quinter21 <anquimon21@gmail.com>
2024-10-30 13:08:27 +0100

    Edicion de nombre de tarjeta

--- WindowsFormsApp1/EnemyCard.Designer.cs
+++ WindowsFormsApp1/EnemyCard.Designer.cs
@@ -28,26 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.Delete = new System.Windows.Forms.Button();
             this.Nombre = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.Delete);
-            this.panel1.Controls.Add(this.Nombre);
-            this.panel1.Location = new System.Drawing.Point(42, 26);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 318);
-            this.panel1.TabIndex = 0;
             // 
             // Delete
             // 
             this.Delete.BackColor = System.Drawing.Color.Red;
-            this.Delete.Location = new System.Drawing.Point(147, 245);
+            this.Delete.Location = new System.Drawing.Point(86, 259);
             this.Delete.Name = "Delete";
             this.Delete.Size = new System.Drawing.Size(127, 49);
             this.Delete.TabIndex = 1;
@@ -57,31 +45,33 @@
             // 
             // Nombre
             // 
+            this.Nombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.Nombre.AutoSize = true;
             this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Nombre.Location = new System.Drawing.Point(164, 118);
+            this.Nombre.Location = new System.Drawing.Point(114, 52);
             this.Nombre.Name = "Nombre";
             this.Nombre.Size = new System.Drawing.Size(79, 29);
             this.Nombre.TabIndex = 0;
             this.Nombre.Text = "label1";
+            this.Nombre.Click += new System.EventHandler(this.Nombre_Click);
             // 
             // EnemyCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Nombre);
             this.Name = "EnemyCard";
-            this.Size = new System.Drawing.Size(487, 378);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Size = new System.Drawing.Size(294, 342);
+            this.DoubleClick += new System.EventHandler(this.EnemyCard_DoubleClick);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label Nombre;
         private System.Windows.Forms.Button Delete;
     }
--- WindowsFormsApp1/EnemyCard.cs
+++ WindowsFormsApp1/EnemyCard.cs
@@ -29,5 +29,46 @@ namespace WindowsFormsApp1
             this.Controls.Clear();
             this.Dispose();
         }
+
+        private void EnemyCard_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wiii");
+        }
+
+        private void Nombre_Click(object sender, EventArgs e)
+        {
+            if (sender is Label label)
+            {
+                var box = new TextBox();
+                box.Name = "TextField";
+                box.Text = label.Text;
+                box.TextAlign = HorizontalAlignment.Center;
+                box.AutoSize = false;
+                box.Dock = DockStyle.None;
+                box.Size = label.Size;
+                box.Location = this.PointToClient(label.Parent.PointToScreen(label.Location));
+                box.BackColor = Color.White;
+                box.PreviewKeyDown += (a, b) => {
+                    if (b.KeyCode == Keys.Escape)
+                    {
+                        box.Parent.Controls.Remove(box);
+                    }
+                    else if (b.KeyCode == Keys.Enter)
+                    {
+                        if (box.Text != "")
+                        {
+                            label.Text = box.Text;
+                        }
+                        box.Parent.Controls.Remove(box);
+                    }
+                };
+                box.LostFocus += (a, b) => { box.Parent?.Controls.Remove(box); };
+
+                Controls.Add(box);
+                Controls.SetChildIndex(box, 0);
+                box.SelectAll();
+                box.Focus();
+            }
+        }
     }
 }
